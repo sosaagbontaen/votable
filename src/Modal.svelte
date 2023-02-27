e2d030d455e484f6ec567af707010486815684a4
Samuel Osa-Agbontaen <sosaagbontaen@college.harvard.edu>
2023-02-26 20:27:03 -0500

    Stylized modal - finished part 10

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,12 +1,30 @@
 <script>
-
+let showModal = true
 </script>
 
-<div class="blackdrop">
+{#if showModal}
+<div class="backdrop">
     <div class="modal">
         <p>Lorem Ipsum</p>
     </div>
 </div>
+{/if}
 
+<style>
+    .backdrop{
+        width:100%;
+        height:100%;
+        position: fixed;
+        background: rgba(0,0,0,0.8);
+    }
+    .modal{
+        padding:10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background: darkgreen;
+        color: white;
+    }
 
-<style></style>
+</style>
